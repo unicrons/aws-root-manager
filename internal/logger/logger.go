e522de7a7628f5d6757472f3e4b44ebb808e6e0a
sbldevnet <sbldevnet@gmail.com>
2026-03-12 14:14:42 +0100

    revert: remove logger interface and restore simple global logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -6,15 +6,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// logrusLogger implements the Logger interface using logrus.
-type logrusLogger struct {
-	logger *log.Logger
-}
-
-// New creates a new Logger instance configured from environment variables.
-func New() Logger {
-	logger := log.New()
-
+func init() {
 	lvl, ok := os.LookupEnv("LOG_LEVEL")
 	if !ok {
 		lvl = "error" // default
@@ -24,56 +16,15 @@ func New() Logger {
 	if err != nil {
 		ll = log.DebugLevel
 	}
-	logger.SetLevel(ll)
+	log.SetLevel(ll)
 
 	format, ok := os.LookupEnv("LOG_FORMAT")
 	if ok {
-		setLoggerFormat(logger, format)
+		SetLoggerFormat(format)
 	}
-
-	return &logrusLogger{logger: logger}
-}
-
-func setLoggerFormat(logger *log.Logger, logFormat string) {
-	switch logFormat {
-	case "json":
-		logger.SetFormatter(&log.JSONFormatter{})
-	default:
-		logger.SetFormatter(&log.TextFormatter{})
-	}
-}
-
-func (l *logrusLogger) Trace(funcName, format string, args ...any) {
-	l.logger.WithField("function", funcName).Tracef(format, args...)
-}
-
-func (l *logrusLogger) Debug(funcName, format string, args ...any) {
-	l.logger.WithField("function", funcName).Debugf(format, args...)
-}
-
-func (l *logrusLogger) Info(funcName, format string, args ...any) {
-	l.logger.WithField("function", funcName).Infof(format, args...)
-}
-
-func (l *logrusLogger) Warn(funcName, format string, args ...any) {
-	l.logger.WithField("function", funcName).Warnf(format, args...)
-}
-
-func (l *logrusLogger) Error(funcName string, err error, format string, args ...any) {
-	l.logger.WithField("function", funcName).WithError(err).Errorf(format, args...)
-}
-
-// Global logger functions for backward compatibility
-// These will be deprecated once all code uses dependency injection
-
-var defaultLogger Logger
-
-func init() {
-	defaultLogger = New()
 }
 
 func SetLoggerFormat(logFormat string) {
-	// For backward compatibility with existing code
 	switch logFormat {
 	case "json":
 		log.SetFormatter(&log.JSONFormatter{})
@@ -82,23 +33,23 @@ func SetLoggerFormat(logFormat string) {
 	}
 }
 
-// Wrap logrus with function name - global functions for backward compatibility
+// Wrap logrus with function name
 func Trace(funcName, format string, args ...any) {
-	defaultLogger.Trace(funcName, format, args...)
+	log.WithField("function", funcName).Tracef(format, args...)
 }
 
 func Debug(funcName, format string, args ...any) {
-	defaultLogger.Debug(funcName, format, args...)
+	log.WithField("function", funcName).Debugf(format, args...)
 }
 
 func Info(funcName, format string, args ...any) {
-	defaultLogger.Info(funcName, format, args...)
+	log.WithField("function", funcName).Infof(format, args...)
 }
 
 func Warn(funcName, format string, args ...any) {
-	defaultLogger.Warn(funcName, format, args...)
+	log.WithField("function", funcName).Warnf(format, args...)
 }
 
 func Error(funcName string, err error, format string, args ...any) {
-	defaultLogger.Error(funcName, err, format, args...)
+	log.WithField("function", funcName).WithError(err).Errorf(format, args...)
 }
